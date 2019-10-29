fba75cb1bb0c5f0f212d88d500d0edfd52a97bb0
IcePear-Jzx <icepear.jzx@gmail.com>
2019-10-29 22:39:57 +0800

    finish top250

--- spider.py
+++ spider.py
@@ -1,32 +1,64 @@
-import cfscrape
+import urllib.request
+import random
+import time
 from lxml import etree
 import json
 
 
+headers = [
+    {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'},
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'},
+    {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'}
+]
+
+
+def get_html(url):
+    html = None
+    time.sleep(random.random())
+
+    try:
+        print('Get:', url)
+        req = urllib.request.Request(url, headers=random.choice(headers))
+        html = urllib.request.urlopen(req).read().decode()
+        print('Success:', url)
+    except:
+        print('Error:', url)
+    
+    return html
+
+
 def get_top250_url():
     data = []
-    scraper = cfscrape.create_scraper()
 
     for i in range(0, 250, 25):
-        html = scraper.get("https://book.douban.com/top250?start={}".format(i))
-        selector = etree.HTML(html.content)
+        url = "https://book.douban.com/top250?start={}".format(i)
+
+        html = None
+        while not html:
+            html = get_html(url)
+
+        selector = etree.HTML(html)
         names = selector.xpath('//div[@class="pl2"]/a/@title')
         urls = selector.xpath('//div[@class="pl2"]/a/@href')
         for j in range(25):
             data.append({"bookName": names[j], "bookURL": urls[j]})
 
-    f = open('top250-url.json', 'w')
-    json.dump(data, f, indent=4)
+        print('Finish:', url)
+
+    with open('top250-url.json', 'w') as f:
+        json.dump(data, f, indent=4)
 
 
 def get_top250_detail():
-    f = open('top250-url.json', 'r')
-    books = json.loads(f.read())
-    scraper = cfscrape.create_scraper()
+    with open('top250-url.json', 'r') as f:
+        books = json.loads(f.read())
+
     for book in books:
         # get bookType
-        html = scraper.get(book['bookURL'])
-        selector = etree.HTML(html.content)
+        html = None
+        while not html:
+            html = get_html(book['bookURL'])
+        selector = etree.HTML(html)
         book['bookType'] = selector.xpath(
             '//div[@id="db-tags-section"]/div/span/a/text()')
 
@@ -34,9 +66,10 @@ def get_top250_detail():
         book['shortRemark'] = []
         i = 1
         while True:
-            shortRemarks = scraper.get(
-                book['bookURL'] + 'comments/hot?p={}'.format(i))
-            selector = etree.HTML(shortRemarks.content)
+            shortRemarks = None
+            while not shortRemarks:
+                shortRemarks = get_html(book['bookURL'] + 'comments/hot?p={}'.format(i))
+            selector = etree.HTML(shortRemarks)
             ids = selector.xpath(
                 '//div[@class="comment"]//span[@class="comment-info"]/a/text()')
             starClasses = selector.xpath(
@@ -66,7 +99,6 @@ def get_top250_detail():
             nextButton = selector.xpath(
                 '//ul[@class="comment-paginator"]/li[3]/a/@href')
             if nextButton and i < 1:
-                print('Finish comments page', i)
                 i += 1
             else:
                 break
@@ -75,9 +107,10 @@ def get_top250_detail():
         book['longRemark'] = []
         i = 0
         while True:
-            shortRemarks = scraper.get(
-                book['bookURL'] + 'reviews?start={}'.format(i))
-            selector = etree.HTML(shortRemarks.content)
+            longRemarks = None
+            while not longRemarks:
+                longRemarks = get_html(book['bookURL'] + 'reviews?start={}'.format(i))
+            selector = etree.HTML(longRemarks)
             nextButton = selector.xpath(
                 '//div[@class="paginator"]/span[@class="next"]/a/@href')
             ids = selector.xpath(
@@ -95,8 +128,10 @@ def get_top250_detail():
             # print(ids, upNumbers, downNumbers)
             contents = []
             for url in urls:
-                review = scraper.get(url)
-                selector = etree.HTML(review.content)
+                review = None
+                while not review:
+                    review = get_html(url)
+                selector = etree.HTML(review)
                 content = selector.xpath(
                     """//div[@id="link-report"]/div[@class="review-content clearfix"]/text()|
                     //div[@id="link-report"]/div[@class="review-content clearfix"]/p/text()""")
@@ -106,20 +141,20 @@ def get_top250_detail():
                 book['longRemark'].append(
                     {'id': ids[j], 'content': contents[j], 'starNumber': starNumbers[j],
                      'usefulNumber': upNumbers[j].strip() + '/' + downNumbers[j].strip()})
-
+            
             if nextButton and i < 0:
-                print('Finish reviews number', i)
                 i += 20
             else:
                 break
-        # only 1 book
-        break
+        
+        print('Finish: book', books.index(book))
 
-    f = open('top250-detail.json', 'w')
-    json.dump(books, f, indent=4)
+    with open('top250-detail.json', 'w') as f:
+        json.dump(books, f, indent=4)
 
 
 get_top250_detail()
-f = open('top250-detail.json', 'r')
-books = json.loads(f.read())
-print(books[0])
+# f = open('top250-detail.json', 'r')
+# books = json.loads(f.read())
+# print(books[0])
+# get_top250_url()
