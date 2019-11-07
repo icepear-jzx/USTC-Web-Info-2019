1220cfc77282c740dd1c3888658d9fd1ab2353dd
IcePear-Jzx <icepear.jzx@gmail.com>
2019-11-08 00:11:36 +0800

    update

---
+++ Lab1-Search-Engine/tokenizer.py
@@ -0,0 +1,31 @@
+import pkuseg
+import pandas as pd
+from collections import Counter
+import json
+from tqdm import tqdm
+
+
+def pkuseg_tokenizer():
+    data = pd.read_csv('Lab1-Search-Engine/Data/test_docs.csv')
+    token_count = []
+
+    seg = pkuseg.pkuseg()
+    for i, row in tqdm(list(data.iterrows())):
+        if str(row['doc_title']) == 'nan' or str(row['content']) == 'nan':
+            continue
+        else:
+            text = seg.cut(row['doc_title'] + row['content'])
+        tokens = Counter(text)
+        token_count.append({
+            'doc_id': row['doc_id'], 
+            'doc_url': row['doc_url'], 
+            'doc_title': row['doc_title'],
+            'tokens': tokens
+        })
+
+    with open('Lab1-Search-Engine/Data/docs_token_pkuseg.json', 'w') as f:
+        json.dump(token_count, f, indent=4, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    pkuseg_tokenizer()
