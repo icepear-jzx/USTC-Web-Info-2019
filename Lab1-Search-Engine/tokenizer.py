44d20b0ef21a130586f6d4125a9ece928bf494bd
IcePear-Jzx <icepear.jzx@gmail.com>
2019-11-08 10:58:03 +0800

    querys tokenized

--- Lab1-Search-Engine/tokenizer.py
+++ Lab1-Search-Engine/tokenizer.py
@@ -26,6 +26,22 @@ def pkuseg_tokenizer():
     with open('Lab1-Search-Engine/Data/docs_token_pkuseg.json', 'w') as f:
         json.dump(token_count, f, indent=4, ensure_ascii=False)
 
+    data = pd.read_csv('Lab1-Search-Engine/Data/test_querys.csv')
+    token_count = []
+
+    seg = pkuseg.pkuseg()
+    for i, row in tqdm(list(data.iterrows())):
+        text = seg.cut(row['query'])
+        tokens = Counter(text)
+        token_count.append({
+            'query_id': row['query_id'],
+            'query': row['query'],
+            'tokens': tokens
+        })
+
+    with open('Lab1-Search-Engine/Data/querys_token_pkuseg.json', 'w') as f:
+        json.dump(token_count, f, indent=4, ensure_ascii=False)
+
 
 if __name__ == "__main__":
     pkuseg_tokenizer()
