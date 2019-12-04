416461ad6568cb1279bfb46302f07ec2f7e48a7b
IcePear-Jzx <icepear.jzx@gmail.com>
2019-12-04 11:58:35 +0800

    lab2

---
+++ Lab2-Entity-Recognition/main.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import numpy as np
+import os, argparse, time, random
+from model import BiLSTM_CRF
+
+from utils import str2bool, read_corpus, read_dictionary, tag2label, random_embedding
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.gpu_options.per_process_gpu_memory_fraction = 0.2 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--epoch', type=int, default=100)
+parser.add_argument('--hidden_dim', type=int, default=300)
+parser.add_argument('--optimizer', type=str, default='Adam')
+parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--clip', type=float, default=5.0)
+parser.add_argument('--dropout', type=float, default=0.5)
+parser.add_argument('--update_embedding', type=str2bool, default=True)
+parser.add_argument('--pretrain_embedding', type=str, default='random')
+parser.add_argument('--CRF', type=str2bool, default=True)
+parser.add_argument('--embedding_dim', type=int, default=300)
+parser.add_argument('--shuffle', type=str2bool, default=True)
+parser.add_argument('--train', type=str2bool, default=True)
+args = parser.parse_args()
+
+
+word2id = read_dictionary('word2id.pkl')
+embeddings = random_embedding(word2id, args.embedding_dim)
+
+predict_path = 'predict.csv' #output
+model_path = "saved_model/"
+train_path = 'train_data'
+test_path = 'test_data'     #test_data
+
+train_data = read_corpus(train_path)
+train_size = len(train_data)
+test_data = read_corpus(test_path)
+test_size = len(test_data)
+
+if args.train:
+    model = BiLSTM_CRF(args, embeddings, tag2label, word2id, output_path=predict_path, model_path=model_path, config=config)
+    model.build_graph()
+    model.train(train=train_data)
+
+else:
+    model_path = tf.train.latest_checkpoint(model_path)
+    model = BiLSTM_CRF(args, embeddings, tag2label, word2id, output_path=predict_path, model_path=model_path, config=config)
+    model.build_graph()
+    labels = model.predict(test_data)
---
+++ Lab2-Entity-Recognition/test.py
@@ -0,0 +1,35 @@
+import json
+import pandas as pd
+
+
+df = pd.read_csv('Lab2-Entity-Recognition/Data/submit.csv')
+answer = json.loads(open('Lab2-Entity-Recognition/Data/answer.json').read())
+
+TP = 0
+TN = 0
+FP = 0
+FN = 0
+
+for textId in range(600):
+    mask = df[df['textId'] == textId]
+    labels = mask['label_type'].tolist()
+    starts = mask['start_pos'].tolist()
+    ends = mask['end_pos'].tolist()
+    pre_items = [(labels[i], starts[i], ends[i]) for i in range(len(labels))]
+    true_items = [(item['label_type'], item['start_pos'], item['end_pos']) 
+                        for item in answer[textId]['entities']]
+    for item in pre_items:
+        if item in true_items:
+            TP += 1
+        else:
+            FP += 1
+    for item in true_items:
+        if item not in pre_items:
+            FN += 1
+
+precision = TP/(TP+FP)
+recall = TP/(TP+FN)
+f1 = 2 * precision * recall / (precision + recall)
+print('Precision:', precision)
+print('Recall:', recall)
+print('F1:', f1)
